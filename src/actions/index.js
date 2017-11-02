d0959d2eb5d77963acd71eeac8bbfc59bc28922a
Francy Lang <francy.lang@gmail.com>
2017-11-02 09:30:23 -0600

    Setting up fetch for house data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,21 @@
 export const fakeAction = () => ({type: 'FAKE'});
+
+
+export const fetchHouseData = () => {
+  return dispatch => {
+  houseDataFetcher()
+    .then(res => res.json())
+    .then(resJSON => resJSON.results)
+    .then(moviesArray => MovieDataCleaner(moviesArray))
+    .then(movies => dispatch(fetchDataSuccess(movies)))
+    .catch(() => dispatch(fetchDataError(true)));
+  };
+};
+  }
+}
+
+
+const houseDataFetcher = () => {
+  return fetch(`http://localhost:3001/api/v1/houses
+`);
+};
